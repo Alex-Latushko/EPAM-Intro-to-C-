1c6bbc74a49168e7a23fcce1fd473f111d9c803e
Alex-Latushko <63583308+Alex-Latushko@users.noreply.github.com>
2020-05-16 16:59:33 +0300

    Add pointers hygiene

--- Strings/task3_3_21.cpp
+++ Strings/task3_3_21.cpp
@@ -5,12 +5,43 @@
 #define SIZE 5
 
 template <typename T>
+void check_ptr(T* ptr, int line){
+	if ( ptr == NULL){
+		printf("NULL pointer in line %d!\n", line);
+		exit(1);
+	}
+}
+
+template <typename T>
+void check_ptr_ptr(T** array, size_t size, int line){
+	check_ptr(array, line);
+	for (size_t i = 0; i < size; ++i){
+		check_ptr( *(array + i), line );
+	}
+}
+
+template <typename T>
+void free_ptr(T* ptr){
+	free(ptr);
+	ptr = NULL;
+}
+
+template <typename T>
+void free_ptr_ptr(T** array, size_t size){
+	for (size_t i = 0; i < size; ++i){
+		free_ptr( *(array + i) );
+	}
+	free_ptr(array);
+}
+
+template <typename T>
 bool is_less(T x, T y){
 	return (x < y);
 }
 
 template <typename T>
 void print_array_cout(T** array, size_t size_i, size_t size_j){
+	check_ptr_ptr(array, size_i, __LINE__);
 	printf("\t");
 	for (size_t j = 0; j < size_j; ++j){
 		printf("%d\t", j);
@@ -27,6 +58,7 @@ void print_array_cout(T** array, size_t size_i, size_t size_j){
 
 template <typename T>
 T calc_sum_array(T** array, size_t size_i, size_t size_j, bool func(T x, T y)){
+	check_ptr_ptr(array, size_i, __LINE__);
 	T sum = 0;
 	for (size_t i = 0; i < size_i; ++i){
 		for (size_t j = 0; j < size_j; ++j){
@@ -40,6 +72,7 @@ T calc_sum_array(T** array, size_t size_i, size_t size_j, bool func(T x, T y)){
 
 template <typename T>
 T calc_prod_array(T** array, size_t size_i, size_t size_j, bool func(T x, T y)){
+	check_ptr_ptr(array, size_i, __LINE__);
 	T prod = 1;
 	for (size_t i = 0; i < size_i; ++i){
 		for (size_t j = 0; j < size_j; ++j){
@@ -52,7 +85,7 @@ T calc_prod_array(T** array, size_t size_i, size_t size_j, bool func(T x, T y)){
 }
 
 int main() {
-	double** array =(double**) calloc(SIZE, sizeof(double*));
+	double** array = (double**) calloc(SIZE, sizeof(double*));
 	for (size_t i = 0; i < SIZE; ++i){
 		*(array+i) = (double*) calloc(SIZE, sizeof(double));
 	}
@@ -66,5 +99,7 @@ int main() {
 	print_array_cout(array, SIZE, SIZE);
 	printf("Sum of the elements bellow diagonal of the array: %.2f\n", calc_sum_array(array, SIZE, SIZE, is_less));
 	printf("Product of the elements bellow diagonal of the array: %.2f\n", calc_prod_array(array, SIZE, SIZE, is_less));
+
+	free_ptr_ptr(array, SIZE);
 	return 0;
 }
